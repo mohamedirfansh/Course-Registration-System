02a61dae02f792a4831d293d62f1bc315d407c2f
Mohamed Irfan <irfancuber@gmail.com>
2020-11-18 23:52:00 +0800

    Updated School.java

--- MySTARS/src/entities/School.java
+++ MySTARS/src/entities/School.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.ArrayList;
+
 /**
  * School class unique attributes for a particular school.
  * It contains a list of Students, list of Staff, list of Courses
@@ -8,7 +10,7 @@ package entities;
  */
 public class School {
 	/**
-	 * The fullname of the school
+	 * The full name of the school
 	 */
 	private String schoolName;
 	/**
@@ -22,16 +24,22 @@ public class School {
 	/**
 	 * List of all Students part of the particular school
 	 */
-	private Student[] allStudents;
+	private ArrayList<Student> allStudents;
 	/**
 	 * List of all Staff part of the particular school
 	 */
-	private Staff[] allStaff;
+	private ArrayList<Staff> allStaff;
 	/**
 	 * List of courses that are available for this school
 	 */
-	private Course[] allCourses;
+	private ArrayList<Course> allCourses;
 
+	/**
+	 * Stores an AccessPeriod object which allows us to read, write & validate a School's access period.
+	 */
+	private AccessPeriod accessPeriod; 
+	
+	
 	/**
 	 * School constructor to create school object with the school name and initals
 	 * @param schoolName
@@ -46,7 +54,7 @@ public class School {
 	
 	/**
 	 * Getter method to return the school's initials
-	 * @return schoolInitials: String
+	 * @return schoolInitials
 	 */
 	public String getSchoolInitials(){
 		return schoolInitials;
@@ -54,7 +62,7 @@ public class School {
 
 	/**
 	 * Getter method to return the school's name
-	 * @return schoolName: String
+	 * @return schoolName
 	 */
 	public String getSchoolName(){
 		return schoolName;
@@ -62,7 +70,7 @@ public class School {
 	
 	/**
 	 * Getter method to return the school's ID
-	 * @return schoolID: int
+	 * @return schoolID
 	 */
 	public int getSchoolID(){
 		return schoolID;
@@ -70,26 +78,35 @@ public class School {
 
 	/**
 	 * Getter method to return the school's list of students
-	 * @return allStudents: Student[]
+	 * @return allStudents
 	 */
-	public Student[] getAllStudents(){
+	public ArrayList<Student> getAllStudents(){
 		return allStudents;
 	}
 	
 	/**
 	 * Getter method to return the school's list of staff
-	 * @return allStaff: Staff[]
+	 * @return allStaff
 	 */
-	public Staff[] getAllStaff(){
+	public ArrayList<Staff> getAllStaff(){
 		return allStaff;
 	}
 
 	/**
 	 * Getter method to return the school's list of courses offered
-	 * @return allCourses: Course[]
+	 * @return allCourses
 	 */
-	public Course[] getAllCourses(){
+	public ArrayList<Course> getAllCourses(){
 		return allCourses;
+	}
+	
+	/**
+	 * Receives the access period for the school
+	 */
+	public void getAccessPeriod() { // Should we print here? or at the boundary class and return just start and end dates.
+		String startDate = this.accessPeriod.getStartDate();
+		String endDate = this.accessPeriod.getEndDate();
+		System.out.println(this.schoolName + "'s access period starts on " + startDate + " and ends on " + endDate);
 	}
 
 	// All setter methods
@@ -122,7 +139,7 @@ public class School {
 	 * Setter method to set school's students
 	 * @param allStudents
 	 */
-	public void setAllStudents(Student[] allStudents){
+	public void setAllStudents(ArrayList<Student> allStudents){
 		this.allStudents = allStudents;
 	}
 
@@ -130,7 +147,7 @@ public class School {
 	 * Setter method to set school's staffs
 	 * @param allStaff
 	 */
-	public void setAllStaff(Staff[] allStaff){
+	public void setAllStaff(ArrayList<Staff> allStaff){
 		this.allStaff = allStaff;
 	}
 
@@ -138,8 +155,24 @@ public class School {
 	 * Setter method to set school's courses offered
 	 * @param allCourses
 	 */
-	public void setAllCourses(Course[] allCourses){
+	public void setAllCourses(ArrayList<Course> allCourses){
 		this.allCourses = allCourses;
+	}
+	
+	/**
+	 * Tries to set the access period for the school and returns 
+	 * a boolean depending on its success.
+	 * @param startDate
+	 * @param endDate
+	 * @return isValid
+	 */
+	public boolean setAccessPeriod(String startDate, String endDate) {
+		AccessPeriod updatedAccessPeriod = new AccessPeriod(startDate, endDate);
+		boolean isValid = updatedAccessPeriod.isValidPeriod();
+		if (isValid) {
+			this.accessPeriod = updatedAccessPeriod;
+		} 
+		return isValid;
 	}
 
 }
