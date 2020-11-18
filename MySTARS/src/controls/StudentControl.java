8bab477cfec3a1f4e8605fb760ab0ebd279a3ceb
Mohamed Irfan <irfancuber@gmail.com>
2020-11-18 15:50:07 +0800

    Added StudentControl.java

---
+++ MySTARS/src/controls/StudentControl.java
@@ -0,0 +1,41 @@
+package controls;
+
+import entities.Student;
+
+/**
+ * This is a control class for the Student object.
+ * It contains the methods (main logic) that a 
+ * student entity is supposed to be able to do in the system.
+ */
+public class StudentControl {
+	// Temp for now...
+	private Student currentStudent;
+	
+	/**
+	 * Method that contains logic to add course for a
+	 * student. Called by the student object.
+	 */
+	public static void addCourse() {
+		pass;
+	}
+	
+	public static void dropCourse() {
+		pass;
+	}
+	
+	public static void checkVacancy() {
+		pass;
+	}
+	
+	public static void changeIndex() {
+		pass;
+	}
+	
+	public static void swapIndex() {
+		pass;
+	}
+	
+	public static void isClashBetIndex() {
+		pass;
+	}
+}
