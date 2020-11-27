778a33a19faa7a44ee383e746fff14a61e5902a8
Mohamed Irfan <irfancuber@gmail.com>
2020-11-27 18:02:54 +0800

    Added documentation

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"entities","l":"boundaries"},{"m":"entities","l":"controls"},{"m":"entities","l":"data"},{"m":"entities","l":"entities"}]
